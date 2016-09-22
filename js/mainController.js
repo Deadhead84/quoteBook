60623f4238648fd918176acde252bd590a25a4b7
Deadhead84 <jonscoville@comcast.net>
2016-09-22 17:55:10 -0600

    finsished other than black diamond

--- js/mainController.js
+++ js/mainController.js
@@ -1,3 +1,20 @@
-angular.module('quoteBook').controller('MainCtrl', function($scope) {
-  $scope.test = "Working!";
+angular.module('quoteBook').controller('MainCtrl', function($scope, dataService) {
+
+  $scope.quotes = dataService.getData();
+
+  $scope.deleteMe = function(textToDelete) {
+    dataService.removeData(textToDelete);
+  }
+
+  $scope.addQuote = function() {
+    var newQuote = {
+      text: $scope.newQuoteText,
+      author: $scope.newQuoteAuthor
+    }
+
+    if (dataService.addData(newQuote)) {
+      $scope.newQuoteText = '';
+      $scope.newQuoteAuthor = '';
+    }
+  }
 })
